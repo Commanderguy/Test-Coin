674649636fec86ba441f461fb59101995537ece5
Johannes Janssen <johannes.janssen1@outlook.de>
2020-07-27 20:03:05 +0200

    Added ChainSync project

---
+++ ChainSync/Class1.cs
@@ -0,0 +1,19 @@
+﻿using CoinFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChainSync
+{
+    public class ChainSyncer
+    {
+        Blockchain _chain;
+
+        public ChainSyncer(Blockchain chain)
+        {
+            _chain = chain;
+        }
+    }
+}
