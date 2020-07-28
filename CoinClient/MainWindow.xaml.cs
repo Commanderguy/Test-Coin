2bd796d7ca8a10800d428518410e016d042e8896
Johannes Janssen <johannes.janssen1@outlook.de>
2020-07-28 02:54:53 +0200

    Added base for graphical user interface

--- CoinClient/MainWindow.xaml.cs
+++ CoinClient/MainWindow.xaml.cs
@@ -1,12 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -17,21 +17,84 @@ using CoinFramework;
 namespace CoinClient
 {
     /// <summary>
-    /// Interaktionslogik für MainWindow.xaml
+    /// Show a side panel and the balance of the account
     /// </summary>
     public partial class MainWindow : Window
     {
+        /// <summary>
+        /// Constructor
+        /// </summary>
         public MainWindow()
         {
+            bool nChain = false;
+            if (File.Exists(".blockChain"))
+                chain = Newtonsoft.Json.JsonConvert.DeserializeObject<Blockchain>(File.ReadAllText(".blockChain"));
+            else
+            {
+                chain = new Blockchain();
+                nChain = true;
+            }
+
+            if (File.Exists(".AccountList"))
+            {
+                accounts = Newtonsoft.Json.JsonConvert.DeserializeObject<Dictionary<string, AccountView>>(File.ReadAllText(".AccountList"));
+                current = accounts.First().Value;
+            }
+            else
+            {
+                accounts = new Dictionary<string, AccountView>();
+                accounts.Add("DEBUG-Account", new AccountView());
+                current = accounts.First().Value;
+                File.WriteAllText(".AccountList", Newtonsoft.Json.JsonConvert.SerializeObject(accounts));
+            }
+
+            if(nChain)
+                ForgeNewChain();
+            Save();
             InitializeComponent();
-            AccountView v1 = new AccountView();
-            AccountAddress.Content = ByteArrToString(v1.publicKey, 37);
+            SendButton.Content = "Send " + (string)Environment.CurrencyType + "s";
+
+            
+            AccountAddress.Content = ByteArrToString(current.publicKey, 37);
+            onChainUpdated += UpdateBalance;
+            onChainUpdated();
         }
 
 
+        /// <summary>
+        /// Get environment variables from the Json file.
+        /// </summary>
+        static dynamic Environment = Newtonsoft.Json.JsonConvert.DeserializeObject(File.ReadAllText("env.json"));
 
 
 
+        AccountView current;
+
+        Dictionary<string, AccountView> accounts;
+
+
+        public void ForgeNewChain()
+        {
+            Block b = new CoinFramework.Block();
+            b.prev_hash = null;
+            b.block_number = 0;
+            b.miner = current.publicKey;
+            b.calculateNonce(current.publicKey, chain);
+            chain.AddBlock(b);
+        }
+
+
+        public void Save()
+        {
+            File.WriteAllText(".blockChain", Newtonsoft.Json.JsonConvert.SerializeObject(chain));
+        }
+
+        /// <summary>
+        /// Turn a byte array into a string in hex format.
+        /// </summary>
+        /// <param name="arr">The byte array</param>
+        /// <param name="cPerLiner">Characters per line</param>
+        /// <returns>The array as a string</returns>
         static string ByteArrToString(byte[] arr, int cPerLiner)
         {
             StringBuilder builder = new StringBuilder();
@@ -41,6 +104,55 @@ namespace CoinClient
                 if ((i + 2) % cPerLiner == 0) builder.Append("\n");
             }
             return builder.ToString();
+        }
+
+
+        /// <summary>
+        /// Turn a byte array into a string in hex format.
+        /// </summary>
+        /// <param name="arr">The byte array</param>
+        /// <returns>The array as a string</returns>
+        static string ByteArrToString(byte[] arr)
+        {
+            StringBuilder builder = new StringBuilder();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                builder.Append(arr[i].ToString("x2"));
+            }
+            return builder.ToString();
+        }
+
+        public void UpdateBalance()
+        {
+            Balance.Content = chain.count_funds(current.publicKey).ToString() + (string)Environment.CurrencyType;
+        }
+
+
+        Blockchain chain;
+
+
+        // Delegates
+        public delegate void ChainChangedEvent();
+        public ChainChangedEvent onChainUpdated;
+
+
+        //Events
+
+        private void AccountAddress_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Clipboard.SetText(ByteArrToString(current.publicKey));
+        }
+
+        private void SendButton_Click(object sender, RoutedEventArgs e)
+        {
+            SendInterface.init(ReceiveTransaction, chain, current.privateKey, current.publicKey, (string)Environment.CurrencyType);
+            SendInterface.Visibility = Visibility.Visible;
+        }
+
+
+        public void ReceiveTransaction(Transaction tx)
+        {
+            SendInterface.Visibility = Visibility.Hidden;
         }
     }
 }
---
+++ CoinClient/RequestSendAdress.xaml.cs
@@ -0,0 +1,83 @@
+﻿using CoinFramework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CoinClient
+{
+    /// <summary>
+    /// Interaktionslogik für RequestSendAdress.xaml
+    /// </summary>
+    public partial class RequestSendAdress : UserControl
+    {
+        public RequestSendAdress()
+        {
+            InitializeComponent();
+            
+        }
+
+        public void init(onFinish finishFunc, Blockchain chain, byte[] privKey, byte[] pubKey, string currency)
+        {
+            privateKey = privKey;
+            publicKey = pubKey;
+            finished += finishFunc;
+            
+            SndLabel.Content = "Send " + currency; 
+
+        }
+
+        byte[] privateKey;
+        byte[] publicKey;
+
+        onFinish finished;
+
+        private void ConfirmTx_Click(object sender, RoutedEventArgs e)
+        {
+            Transaction tx = new Transaction(publicKey, StringToByteArray(Rec.Text), Convert.ToDouble(Amount.Text), 0, privateKey);
+            finished(tx);
+        }
+
+        public static byte[] StringToByteArray(String hex)
+        {
+            int NumberChars = hex.Length;
+            byte[] bytes = new byte[NumberChars / 2];
+            for (int i = 0; i < NumberChars; i += 2)
+                bytes[i / 2] = Convert.ToByte(hex.Substring(i, 2), 16);
+            return bytes;
+        }
+
+
+        public delegate void onFinish(Transaction tx);
+
+        private void Amount_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (Amount.Text == "") return;
+
+            var n = Amount.CaretIndex;
+            char nChar = Amount.Text[Amount.Text.Length - 1];
+            if(char.IsDigit(nChar) || nChar == '.' || nChar == ',')
+            {
+                return;
+            }
+            Amount.Text = Amount.Text.Substring(0, Amount.Text.Length - 1);
+            Amount.CaretIndex = n;
+        }
+
+        private void cncl_Click(object sender, RoutedEventArgs e)
+        {
+            finished(null);
+        }
+    }
+}
